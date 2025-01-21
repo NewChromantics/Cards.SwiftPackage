6e19b600cbb6652661084e3f90d8d8189730846a
Graham Reeves <graham@grahamreeves.com>
2025-01-21 23:51:08 +0000

    Added CardSuits.randomSuit

--- Source/Card.swift
+++ Source/Card.swift
@@ -405,6 +405,11 @@ public class CardSuit
 	static public let spade = "suit.spade.fill"
 	static public let club = "suit.club.fill"
 	static public let diamond = "suit.diamond.fill"
+	
+	static public var randomSuit : String
+	{
+		return [heart,spade,club,diamond].randomElement()!
+	}
 }
 
 struct CardStyle
